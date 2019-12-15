ff024b9c0b7d98712e644de5ec2b939e4756db32
NesquikPlus <bedoozcan@gmail.com>
2019-12-15 15:32:12 +0300

    wrong commit

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main()
   
     GameObject g1("objFolder/ball/ball.obj", glm::vec3(5.0f, 0.4f, -0.3f), glm::vec3(-1.0f, -0.1f, 0.1f), 10);
     GameObject g2("objFolder/ball/ball.obj", glm::vec3(-5.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.1f, 0.2f), 50);
-    GameObject g3("objFolder/ball/ball.obj", glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.5f, 0.1f, 0.2f), 30);
+    GameObject g3("objFolder/ball/ball.obj", glm::vec3(1.0f, 0.0f, -1.0f), glm::vec3(0.5f, 0.1f, 0.2f), 30);
 
     game.AddGameObject(g1);
     game.AddGameObject(g2);
-
+    game.AddGameObject(g3);
 
     game.Start();
     
